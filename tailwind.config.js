6ad843c5fa0fb693682321a58454c17d9f6cb11e
Kaom123 <78056145+Kaom123@users.noreply.github.com>
2025-01-15 20:32:39 -0500

    kaosdm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
    
